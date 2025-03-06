88e8c253f9d381aadd77554cf3ab8f38d955f360
TheCactusBlue <thecactusblue@gmail.com>
2025-03-06 12:49:56 -0800

    fix migration

--- apps/backend/prisma/migrations/20250304004231_merge_oauth_methods/migration.sql
+++ apps/backend/prisma/migrations/20250304004231_merge_oauth_methods/migration.sql
@@ -2,4 +2,4 @@
 CREATE TYPE "MergeOAuthMethods" AS ENUM ('LINK_METHOD', 'RAISE_ERROR', 'ALLOW_DUPLICATES');
 
 -- AlterTable
-ALTER TABLE "ProjectConfig" ADD COLUMN "mergeOAuthMethods" "MergeOauthMethods" NOT NULL DEFAULT 'LINK_METHOD';
+ALTER TABLE "ProjectConfig" ADD COLUMN "mergeOAuthMethods" "MergeOAuthMethods" NOT NULL DEFAULT 'LINK_METHOD';
