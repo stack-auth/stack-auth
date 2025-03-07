7149043de8bbf9966dc3e516f760188f324b021a
TheCactusBlue <thecactusblue@gmail.com>
2025-03-07 01:53:54 -0800

    fixed

--- apps/dashboard/src/components/project-card.tsx
+++ apps/dashboard/src/components/project-card.tsx
@@ -2,13 +2,14 @@
 import { Link } from "@/components/link";
 import { useFromNow } from '@/hooks/use-from-now';
 import { AdminProject } from '@stackframe/stack';
+import { urlString } from "@stackframe/stack-shared/dist/utils/urls";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle, Typography } from '@stackframe/stack-ui';
 
 export function ProjectCard({ project }: { project: AdminProject }) {
   const createdAt = useFromNow(project.createdAt);
 
   return (
-    <Link href={`/projects/${project.id}`}>
+    <Link href={urlString`/projects/${project.id}`}>
       <Card className='flex flex-col justify-between'>
         <CardHeader>
           <CardTitle className="normal-case truncate">{project.displayName}</CardTitle>
