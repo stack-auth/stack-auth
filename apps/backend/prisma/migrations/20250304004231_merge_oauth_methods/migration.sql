f543f804e28f33d0c81041ca26d835040da98857
TheCactusBlue <thecactusblue@gmail.com>
2025-03-07 10:37:54 -0800

    modify migration for legacy projects

--- apps/backend/prisma/migrations/20250304004231_merge_oauth_methods/migration.sql
+++ apps/backend/prisma/migrations/20250304004231_merge_oauth_methods/migration.sql
@@ -3,3 +3,6 @@ CREATE TYPE "OAuthAccountMergeStrategy" AS ENUM ('LINK_METHOD', 'RAISE_ERROR', '
 
 -- AlterTable
 ALTER TABLE "ProjectConfig" ADD COLUMN "oauthAccountMergeStrategy" "OAuthAccountMergeStrategy" NOT NULL DEFAULT 'LINK_METHOD';
+
+-- Update existing projects to use the new strategy
+UPDATE "ProjectConfig" SET "oauthAccountMergeStrategy" = 'ALLOW_DUPLICATES';
