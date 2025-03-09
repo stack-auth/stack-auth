aae4061f05a1356942408ec8e4791e464d25d9d9
TheCactusBlue <thecactusblue@gmail.com>
2025-03-09 16:06:55 -0700

    use typedToUpper/Lowercase

--- apps/backend/src/app/api/latest/projects/current/crud.tsx
+++ apps/backend/src/app/api/latest/projects/current/crud.tsx
@@ -468,9 +468,7 @@ export const projectsCrudHandlers = createLazyProxy(() => createCrudHandlers(pro
               clientUserDeletionEnabled: data.config?.client_user_deletion_enabled,
               allowLocalhost: data.config?.allow_localhost,
               createTeamOnSignUp: data.config?.create_team_on_sign_up,
-              oauthAccountMergeStrategy: data.config?.oauth_account_merge_strategy ?
-                typedToUppercase(data.config.oauth_account_merge_strategy) as 'LINK_METHOD' | 'RAISE_ERROR' | 'ALLOW_DUPLICATES' :
-                undefined,
+              oauthAccountMergeStrategy: data.config?.oauth_account_merge_strategy ? typedToUppercase(data.config.oauth_account_merge_strategy) : undefined,
               domains: data.config?.domains ? {
                 deleteMany: {},
                 create: data.config.domains.map(item => ({
