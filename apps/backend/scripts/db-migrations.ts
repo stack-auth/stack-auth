f09a2a1187eb1823927e31613a3e44d4d48e4541
Zai Shi <zaishi00@outlook.com>
2025-03-10 08:14:57 -0700

    improved db command prompting

--- apps/backend/scripts/db-migrations.ts
+++ apps/backend/scripts/db-migrations.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "@/prisma-client";
 import { applyMigrations, refreshMigrationFiles } from "@/utils/auto-migration";
 import { execSync } from "child_process";
+import * as readline from 'readline';
 
 const dropPublicSchema = async () => {
   await prismaClient.$executeRawUnsafe(`DROP SCHEMA public CASCADE`);
@@ -13,28 +14,30 @@ const seed = async () => {
   execSync('pnpm run db-seed-script', { stdio: 'inherit' });
 };
 
+const getDropDBPrompt = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const answer = await new Promise<string>(resolve => {
+    rl.question('Are you sure you want to drop everything in the database? This action cannot be undone. (Y/n): ', resolve);
+  });
+  rl.close();
+
+  if (answer.toLowerCase() !== 'y' && answer !== '') {
+    console.log('Operation cancelled');
+    process.exit(0);
+  }
+};
+
 const main = async () => {
   const args = process.argv.slice(2);
   const command = args[0];
 
   switch (command) {
     case 'reset': {
-      const readline = require('readline');
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-
-      const answer = await new Promise<string>(resolve => {
-        rl.question('Are you sure you want to drop everything in the database? This action cannot be undone. (Y/n): ', resolve);
-      });
-      rl.close();
-
-      if (answer.toLowerCase() !== 'y' && answer !== '') {
-        console.log('Operation cancelled');
-        process.exit(0);
-      }
-
+      await getDropDBPrompt();
       await dropPublicSchema();
       refreshMigrationFiles();
       await applyMigrations({ appliedMigrationsIfNoMigrationTable: [] });
