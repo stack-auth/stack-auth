72d8dd28229104f5485c5beb5235f758766155d0
Zai Shi <zaishi00@outlook.com>
2025-03-14 18:03:16 -0700

    fixed small problems

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -1,24 +1,25 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vitest/config'
 import { loadEnv } from 'vite'
+import { defineConfig, mergeConfig } from 'vitest/config'
 import sharedConfig from '../../vitest.shared'
 
-export default defineConfig({
-  test: {
-    testTimeout: 20000,
-    include: sharedConfig.test?.include,
-    includeSource: sharedConfig.test?.includeSource,
-    environment: 'jsdom',
-    env: {
-      ...loadEnv('', process.cwd(), ''),
-      ...loadEnv('development', process.cwd(), ''),
+export default mergeConfig(
+  sharedConfig,
+  defineConfig({
+    test: {
+      testTimeout: 20000,
+      environment: 'jsdom',
+      env: {
+        ...loadEnv('', process.cwd(), ''),
+        ...loadEnv('development', process.cwd(), ''),
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  envDir: __dirname,
-  envPrefix: 'STACK_',
-})
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    envDir: __dirname,
+    envPrefix: 'STACK_',
+  })
+)
