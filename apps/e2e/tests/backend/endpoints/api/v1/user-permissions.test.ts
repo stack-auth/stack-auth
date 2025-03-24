7827a1a135362bf4fe842b63e6b262dfcd23d4e7
TheCactusBlue <thecactusblue@gmail.com>
2025-03-24 13:33:55 -0700

    more tests

--- apps/e2e/tests/backend/endpoints/api/v1/user-permissions.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/user-permissions.test.ts
@@ -1,5 +1,6 @@
+import { wait } from "@stackframe/stack-shared/dist/utils/promises";
 import { it } from "../../../../helpers";
-import { Auth, niceBackendFetch } from "../../../backend-helpers";
+import { ApiKey, Auth, InternalProjectKeys, Project, Team, Webhook, backendContext, niceBackendFetch } from "../../../backend-helpers";
 
 it("is not allowed to list permissions from the other users on the client", async ({ expect }) => {
   await Auth.Otp.signIn();
@@ -17,7 +18,7 @@ it("is not allowed to list permissions from the other users on the client", asyn
   `);
 });
 
-it("is not allowed to list permissions from the other users on the server", async ({ expect }) => {
+it("is not allowed to grant non-existing permission to a user on the server", async ({ expect }) => {
   const { userId } = await Auth.Otp.signIn();
 
   const response = await niceBackendFetch(`/api/v1/user-permissions/${userId}/does_not_exist`, {
@@ -25,7 +26,6 @@ it("is not allowed to list permissions from the other users on the server", asyn
     method: "POST",
     body: {},
   });
-  console.log(response);
   expect(response).toMatchInlineSnapshot(`
     NiceResponse {
       "status": 404,
@@ -40,4 +40,122 @@ it("is not allowed to list permissions from the other users on the server", asyn
       },
     }
   `);
+});
+
+it("can create a new permission and grant it to a user on the server", async ({ expect }) => {
+  backendContext.set({ projectKeys: InternalProjectKeys });
+  const { adminAccessToken } = await Project.createAndGetAdminToken({ config: { magic_link_enabled: true } });
+
+  // create a permission child
+  await niceBackendFetch(`/api/v1/team-permission-definitions`, {
+    accessType: "admin",
+    method: "POST",
+    body: {
+      id: 'child',
+      description: 'Child permission',
+    },
+    headers: {
+      'x-stack-admin-access-token': adminAccessToken
+    },
+  });
+
+  // create a permission parent
+  await niceBackendFetch(`/api/v1/team-permission-definitions`, {
+    accessType: "admin",
+    method: "POST",
+    body: {
+      id: 'parent',
+      description: 'Parent permission',
+      contained_permission_ids: ['child'],
+    },
+    headers: {
+      'x-stack-admin-access-token': adminAccessToken
+    },
+  });
+
+  await ApiKey.createAndSetProjectKeys(adminAccessToken);
+
+  const { userId } = await Auth.Password.signUpWithEmail({ password: 'test1234' });
+
+  // list current permissions
+  const response1 = await niceBackendFetch(`/api/v1/user-permissions?user_id=me`, {
+    accessType: "client",
+    method: "GET",
+  });
+  expect(response1).toMatchInlineSnapshot(`
+    NiceResponse {
+      "status": 200,
+      "body": {
+        "is_paginated": false,
+        "items": [],
+      },
+      "headers": Headers { <some fields may have been hidden> },
+    }
+  `);
+
+  // grant new permission
+  const response2 = await niceBackendFetch(`/api/v1/user-permissions/${userId}/parent`, {
+    accessType: "server",
+    method: "POST",
+    body: {},
+  });
+  expect(response2).toMatchInlineSnapshot(`
+    NiceResponse {
+      "status": 201,
+      "body": {
+        "id": "parent",
+        "user_id": "<stripped UUID>",
+      },
+      "headers": Headers { <some fields may have been hidden> },
+    }
+  `);
+
+  // list current permissions (should have the new permission)
+  const response3 = await niceBackendFetch(`/api/v1/user-permissions?user_id=me`, {
+    accessType: "client",
+    method: "GET",
+  });
+  expect(response3).toMatchInlineSnapshot(`
+    NiceResponse {
+      "status": 200,
+      "body": {
+        "is_paginated": false,
+        "items": [
+          {
+            "id": "parent",
+            "user_id": "<stripped UUID>",
+          },
+        ],
+      },
+      "headers": Headers { <some fields may have been hidden> },
+    }
+  `);
+});
+
+// TODO: add user default permissions to the project config
+it.todo("can customize default user permissions", async ({ expect }) => {
+  await Auth.Otp.signIn();
+  const { adminAccessToken } = await Project.createAndGetAdminToken();
+
+  const response1 = await niceBackendFetch(`/api/v1/team-permission-definitions`, {
+    accessType: "admin",
+    method: "POST",
+    body: {
+      id: 'test'
+    },
+    headers: {
+      'x-stack-admin-access-token': adminAccessToken
+    },
+  });
+  expect(response1).toMatchInlineSnapshot();
+
+  const { updateProjectResponse: response2 } = await Project.updateCurrent(adminAccessToken, {
+    config: {
+      // user_default_permissions: [{ id: 'test' }],
+    },
+  });
+
+  await ApiKey.createAndSetProjectKeys(adminAccessToken);
+
+  expect(response2).toMatchInlineSnapshot();
 });
