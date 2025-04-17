fcc8b5eaba381c44f30233b4a0502e6f24c73da9
Zai Shi <zaishi00@outlook.com>
2025-04-17 10:36:54 -0700

    Refactor project CRUD handlers by removing unused imports and simplifying project creation logic. Eliminate default sign-up disabling logic to streamline project initialization.

--- apps/backend/src/app/api/latest/internal/projects/crud.tsx
+++ apps/backend/src/app/api/latest/internal/projects/crud.tsx
@@ -1,4 +1,4 @@
-import { createOrUpdateProject, fullProjectInclude, listManagedProjectIds, projectPrismaToCrud } from "@/lib/projects";
+import { createOrUpdateProject, listManagedProjectIds } from "@/lib/projects";
 import { prismaClient } from "@/prisma-client";
 import { createCrudHandlers } from "@/route-handlers/crud-handler";
 import { KnownErrors } from "@stackframe/stack-shared";
@@ -9,13 +9,6 @@ import { createLazyProxy } from "@stackframe/stack-shared/dist/utils/proxies";
 
 // if one of these users creates a project, the others will be added as owners
 const ownerPacks: Set<string>[] = [];
-
-// if the user is in this list, the project will not have sign-up enabled on creation
-const disableSignUpByDefault = new Set([
-  "c2c03bd1-5cbe-4493-8e3f-17d1e2d7ca43",
-  "60b859bf-e148-4eff-9985-fe6e31c58a2a",
-  "1343e3e7-dd7a-44a1-8752-701c0881da72",
-]);
 
 export const adminUserProjectsCrudHandlers = createLazyProxy(() => createCrudHandlers(adminUserProjectsCrud, {
   paramsSchema: yupObject({
@@ -34,12 +27,11 @@ export const adminUserProjectsCrudHandlers = createLazyProxy(() => createCrudHan
     const ownerPack = ownerPacks.find(p => p.has(user.id));
     const userIds = ownerPack ? [...ownerPack] : [user.id];
 
-    return await createOrUpdateProject(userIds, {
-      ...data,
-      config: {
-        ...data.config,
-        sign_up_enabled: data.config?.sign_up_enabled ?? (disableSignUpByDefault.has(user.id) ? false : true),
-      },
+    return await createOrUpdateProject({
+      ownerIds: userIds,
+      branchId: 'main',
+      type: 'create',
+      data,
     });
   },
   onList: async ({ auth }) => {
@@ -47,7 +39,6 @@ export const adminUserProjectsCrudHandlers = createLazyProxy(() => createCrudHan
       where: {
         id: { in: listManagedProjectIds(auth.user ?? throwErr('auth.user is required')) },
       },
-      include: fullProjectInclude,
       orderBy: { createdAt: 'desc' },
     });
 
