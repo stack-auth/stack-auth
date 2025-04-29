5fb4f5d20d3081c4466ca987209f86553d8b2410
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-04-28 21:25:58 -0700

    fix

--- configs/tsup/js-library.ts
+++ configs/tsup/js-library.ts
@@ -1,13 +1,11 @@
-import fs from 'fs';
-import path from 'path';
 import { defineConfig } from 'tsup';
+import { createBasePlugin } from './plugins';
 
 const customNoExternal = new Set([
   "oauth4webapi",
 ]);
 
 export default function createJsLibraryTsupConfig(options: { barrelFile: boolean }) {
-  const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
   return defineConfig({
     entryPoints: ['src/**/*.(ts|tsx|js|jsx)'],
     sourcemap: true,
@@ -18,44 +16,7 @@ export default function createJsLibraryTsupConfig(options: { barrelFile: boolean
     format: ['esm', 'cjs'],
     legacyOutput: true,
     esbuildPlugins: [
-      {
-        name: 'stackframe tsup plugin (private)',
-        setup(build) {
-          build.onEnd(result => {
-            const sourceFiles = result.outputFiles?.filter(file => !file.path.endsWith('.map')) ?? [];
-            for (const file of sourceFiles) {
-              let newText = file.text;
-
-              // make sure "use client" is at the top of the file
-              const matchUseClient = /[\s\n\r]*(^|\n|\r|;)\s*['"]use\s+client['"]\s*(\n|\r|;)/im;
-              if (matchUseClient.test(file.text)) {
-                newText = `"use client";\n${file.text}`;
-              }
-
-              file.contents = new TextEncoder().encode(newText);
-            }
-          });
-  
-          build.onResolve({ filter: /^.*$/m }, async (args) => {
-            if (args.kind === "entry-point" || customNoExternal.has(args.path)) {
-              return undefined;
-            }
-            return {
-              external: true,
-            };
-          });
-  
-          build.onLoad({ filter: /\.(jsx?|tsx?)$/ }, async (args) => {
-            let contents = await fs.promises.readFile(args.path, 'utf8');
-            contents = contents.replace(/STACK_COMPILE_TIME_CLIENT_PACKAGE_VERSION_SENTINEL/g, `js ${packageJson.name}@${packageJson.version}`);
-            contents = contents.replace(/import\.meta\.vitest/g, 'undefined');
-            return {
-              contents,
-              loader: path.extname(args.path).slice(1) as 'js' | 'jsx' | 'ts' | 'tsx'
-            };
-          });
-        },
-      },
+      createBasePlugin({ customNoExternal }),
     ],
   });
 }
