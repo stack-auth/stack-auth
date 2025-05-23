553cb34a3e4ebe8c7804a93fb278c9191001aaa4
Madison Kennedy <madison.w.kennedy@gmail.com>
2025-05-23 12:42:18 -0500

    Now using dynamic docs layout which allows for sidebar to handle dynamic links based on language

--- docs-fuma-test/stack-docs/src/app/docs/layout.tsx
+++ docs-fuma-test/stack-docs/src/app/docs/layout.tsx
@@ -1,13 +1,12 @@
-import { baseOptions } from '@/app/layout.config';
-import { DocsLayout } from '@/components/layouts/docs';
+import { DynamicDocsLayout } from '@/components/layouts/dynamic-docs';
 import { source } from '@/lib/source';
 import type { ReactNode } from 'react';
 import './custom-docs-styles.css';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DynamicDocsLayout tree={source.pageTree}>
       {children}
-    </DocsLayout>
+    </DynamicDocsLayout>
   );
 }
