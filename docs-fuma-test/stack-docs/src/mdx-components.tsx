ef250f1b22d41daab76fa65c99af205bb4c8ac1b
Madison Kennedy <madison.w.kennedy@gmail.com>
2025-05-27 19:19:31 +0000

    Support for new components

--- docs-fuma-test/stack-docs/src/mdx-components.tsx
+++ docs-fuma-test/stack-docs/src/mdx-components.tsx
@@ -1,21 +1,26 @@
 import * as CodeBlock from 'fumadocs-ui/components/codeblock';
+import { TypeTable } from 'fumadocs-ui/components/type-table';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
 // OpenAPI sources
-import { openapi } from '../lib/source';
+import { APIPage } from 'fumadocs-openapi/ui';
 
 // Raw @stackframe/stack components
 import { SignIn } from '@stackframe/stack';
 
 // Custom components from @stackframe/stack
+import { AccountSettingsStackAuth } from './components/stack-auth/account-settings';
 import { SignInExtraInfo, SignInPasswordFirstTab, SignInStackAuth } from './components/stack-auth/sign-in';
+import { StackUserButton } from './components/stack-auth/stack-user-button';
+import { UserButtonDemo } from './components/stack-auth/user-button-demo';
 
-// Custom components
 import { Card, CardGroup, Info } from './components/mdx';
 import { AuthCard } from './components/mdx/AuthCard';
+import { Accordion, CodeBlocks, Icon, Markdown, ParamField } from './components/mdx/SDKComponents';
+import { PropTable } from './components/PropTable';
 import { Step, Steps } from './components/steps';
-import { Tab, Tabs } from './components/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -30,11 +35,24 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     SignInStackAuth,
     SignInPasswordFirstTab,
     AuthCard,
+    AccountSettingsStackAuth,
     SignInExtraInfo,
+    StackUserButton,
+    UserButtonDemo,
     Steps,
     Step,
     Tabs,
-    Tab,
-    APIPage: openapi.APIPage,
+    TabsList,
+    TabsTrigger,
+    TabsContent,
+    APIPage,
+    TypeTable,
+    PropTable,
+    // SDK Documentation Components
+    Markdown,
+    ParamField,
+    Accordion,
+    CodeBlocks,
+    Icon,
   };
 }
