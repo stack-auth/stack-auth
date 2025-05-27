07eb6855cfffdeedc00e99618393156e1c792642
Madison Kennedy <madison.w.kennedy@gmail.com>
2025-05-27 20:32:52 +0000

    [Feat]: Mock mode in components

--- packages/template/src/components-page/account-settings/settings/settings-page.tsx
+++ packages/template/src/components-page/account-settings/settings/settings-page.tsx
@@ -3,7 +3,9 @@ import { DeleteAccountSection } from "./delete-account-section";
 import { SignOutSection } from "./sign-out-section";
 
 
-export function SettingsPage() {
+export function SettingsPage(props?: {
+  mockMode?: boolean
+}) {
   return (
     <PageLayout>
       <DeleteAccountSection />
