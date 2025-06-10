30efbcc59d34c94e3f6ad69e829ab2a9d89a8fe1
Madison Kennedy <madison.w.kennedy@gmail.com>
2025-06-10 11:36:56 +0000

    included new components

--- docs-fuma-test/stack-docs/src/mdx-components.tsx
+++ docs-fuma-test/stack-docs/src/mdx-components.tsx
@@ -6,28 +6,26 @@ import type { MDXComponents } from 'mdx/types';
 // OpenAPI sources
 import { APIPage } from 'fumadocs-openapi/ui';
 
-// Raw @stackframe/stack components
-//import { SignIn } from '@stackframe/stack';
-
-// Custom components from @stackframe/stack
 import { Card, CardGroup, Info } from './components/mdx';
 import ApiSequenceDiagram from './components/mdx/api-sequence-diagram';
 import { AuthCard } from './components/mdx/AuthCard';
 import { DynamicCodeblock } from './components/mdx/DynamicCodeblock';
-import { Accordion, AccordionGroup, CodeBlocks, Icon, Markdown, ParamField } from './components/mdx/SDKComponents';
-import { PropTable } from './components/PropTable';
 
+import { CollapsibleMethodSection, CollapsibleTypesSection, MethodAside, MethodContent, MethodLayout, MethodSection, MethodTitle } from './components/ui/method-layout';
+
+import { SDKOverview } from './components/sdk/overview';
+
+import { Mermaid } from './components/mdx/mermaid';
+import { Accordion, AccordionGroup, ClickableTableOfContents, CodeBlocks, Icon, Markdown, ParamField } from './components/mdx/SDKComponents';
+import { PropTable } from './components/PropTable';
 
 import { AccountSettingsStackAuth } from './components/stack-auth/account-settings';
 import { SignInDemo, SignInExtraInfo, SignInPasswordFirstTab, SignInStackAuth } from './components/stack-auth/sign-in';
 import { StackUserButton } from './components/stack-auth/stack-user-button';
 import { TeamSwitcherDemo } from './components/stack-auth/team-switcher';
 import { UserButtonDemo } from './components/stack-auth/user-button-demo';
-
-import { Mermaid } from './components/mdx/mermaid';
 import { Step, Steps } from './components/steps';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
-// use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
@@ -65,5 +63,14 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Icon,
     DynamicCodeblock,
     Mermaid,
+    MethodLayout,
+    MethodContent,
+    MethodAside,
+    MethodTitle,
+    MethodSection,
+    ClickableTableOfContents,
+    CollapsibleMethodSection,
+    CollapsibleTypesSection,
+    SDKOverview
   } as MDXComponents;
 }
