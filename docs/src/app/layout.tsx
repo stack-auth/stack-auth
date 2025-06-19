e308c8b92ae6fdf95924e43189ec804ff5bc1633
Madison <madison.w.kennedy@gmail.com>
2025-06-19 18:50:36 -0500

    Try removed stackprovider since we use mock components

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,7 +1,6 @@
-import { StackProvider, StackTheme } from '@stackframe/stack';
+import { StackTheme } from '@stackframe/stack';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
-import { stackServerApp } from '../stack';
 import './global.css';
 
 const inter = Inter({
@@ -17,11 +16,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         ` }} />
       </head>
       <body className="flex flex-col min-h-screen">
-        <StackProvider app={stackServerApp}>
-          <StackTheme>
-            <RootProvider>{children}</RootProvider>
-          </StackTheme>
-        </StackProvider>
+        <StackTheme>
+          <RootProvider>{children}</RootProvider>
+        </StackTheme>
       </body>
     </html>
   );
