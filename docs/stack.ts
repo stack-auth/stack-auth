4dbd19d23accea039f29d897405390203a21fa6b
Madison <madison.w.kennedy@gmail.com>
2025-06-19 18:31:33 -0500

    Remove redundant stack.ts file

--- docs/stack.ts
+++
@@ -1,5 +0,0 @@
-import { StackServerApp } from "@stackframe/stack";
-import "server-only";
-export const stackServerApp = new StackServerApp({
-  tokenStore: "nextjs-cookie", // storing auth tokens in cookies
-});
