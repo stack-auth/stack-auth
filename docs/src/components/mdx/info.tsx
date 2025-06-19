caeff09710cf0b033b59c74ee7fc1d7f75997e62
Madison <madison.w.kennedy@gmail.com>
2025-06-18 20:19:06 -0500

    aligned baseline to first line of text

--- docs/src/components/mdx/info.tsx
+++ docs/src/components/mdx/info.tsx
@@ -40,8 +40,8 @@ export function Info({ children, type = 'info' }: InfoProps) {
       colors.border,
       colors.bg
     )}>
-      <div className="flex p-4">
-        <div className={cn("flex-shrink-0 mr-3 mt-1", colors.icon)}>
+      <div className="flex items-baseline py-1 px-2">
+        <div className={cn("flex-shrink-0 mr-3", colors.icon)}>
           {type === 'info' && (
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
               <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
