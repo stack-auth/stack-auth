09c046b5b7d7a3f838862178bff9bb42e0e58ebf
Zai Shi <zaishi00@outlook.com>
2025-06-21 14:52:11 +0800

    Update migration file extension and script to use .tsx

--- apps/backend/scripts/generate-migration-imports.ts
+++ apps/backend/scripts/generate-migration-imports.ts
@@ -4,6 +4,7 @@ import { MIGRATION_FILES_DIR, getMigrationFiles } from '../src/auto-migrations/u
 
 const migrationFiles = getMigrationFiles(MIGRATION_FILES_DIR);
 
-writeFileSyncIfChanged(path.join(process.cwd(),  'src', 'auto-migrations', 'migration-files.ts'), `
-export const MIGRATION_FILES = ${JSON.stringify(migrationFiles, null, 2)};
-`);
+writeFileSyncIfChanged(
+  path.join(process.cwd(),  'src', 'auto-migrations', 'migration-files.tsx'),
+  `export const MIGRATION_FILES = ${JSON.stringify(migrationFiles, null, 2)};`
+);
