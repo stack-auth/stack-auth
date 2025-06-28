c1eb0a6fc191f4bd8a333c835891b4bb82e1957f
Zai Shi <zaishi00@outlook.com>
2025-06-28 15:27:06 -0700

    temporarily comment out global

--- apps/dashboard/src/polyfills.tsx
+++ apps/dashboard/src/polyfills.tsx
@@ -10,12 +10,12 @@ const sentryErrorSink = (location: string, error: unknown) => {
 export function ensurePolyfilled() {
   registerErrorSink(sentryErrorSink);
 
-  if ("addEventListener" in globalThis) {
-    globalThis.addEventListener("unhandledrejection", (event) => {
-      captureError("unhandled-browser-promise-rejection", event.reason);
-      console.error("Unhandled promise rejection", event.reason);
-    });
-  }
+  // if ("addEventListener" in globalThis) {
+  //   globalThis.addEventListener("unhandledrejection", (event) => {
+  //     captureError("unhandled-browser-promise-rejection", event.reason);
+  //     console.error("Unhandled promise rejection", event.reason);
+  //   });
+  // }
 
   // not all environments have default options for util.inspect
   if ("inspect" in util && "defaultOptions" in util.inspect) {
@@ -28,9 +28,9 @@ export function ensurePolyfilled() {
       if (getNodeEnvironment() === "development") {
         console.error("Unhandled promise rejection. Some production environments (particularly Vercel) will kill the server in this case, so the server will now exit. Please use the `ignoreUnhandledRejection` function to signal that you've handled the error.", reason);
       }
-      if ((globalThis.process as any).exit) {
-        globalThis.process.exit(1);
-      }
+      // if ((globalThis.process as any).exit) {
+      //   globalThis.process.exit(1);
+      // }
     });
   }
 }
