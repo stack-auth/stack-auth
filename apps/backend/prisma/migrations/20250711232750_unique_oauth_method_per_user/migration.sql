21266ea635c4362086c075d22e653fb0f11983d7
Zai Shi <zaishi00@outlook.com>
2025-07-15 16:13:01 -0700

    updated migration file

--- apps/backend/prisma/migrations/20250711232750_unique_oauth_method_per_user/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[tenancyId,projectUserId,configOAuthProviderId]` on the table `OAuthAuthMethod` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "OAuthAuthMethod_tenancyId_projectUserId_configOAuthProvider_key" ON "OAuthAuthMethod"("tenancyId", "projectUserId", "configOAuthProviderId");
