408e38773cb30781426fd0fde24bdaf1843f742e
Zai Shi <zaishi00@outlook.com>
2025-07-28 14:40:59 -0700

    added todos

---
+++ apps/e2e/tests/backend/endpoints/api/v1/internal/environment-config.test.ts
@@ -0,0 +1,20 @@
+import { it } from "../../../../../helpers";
+
+
+it.todo("client and server should not have access to environment config");
+
+it.todo("gets environment config");
+
+it.todo("updates basic environment config");
+
+it.todo("adds, updates, and removes oauth config");
+
+it.todo("misconfigures oauth config");
+
+it.todo("adds, updates, and removes domains");
+
+it.todo("misconfigures domains");
+
+it.todo("adds, updates, and removes email config");
+
+it.todo("misconfigures email config");
