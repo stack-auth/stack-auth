68ea382659796bc44abf17eb3626aed1e0035967
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-07-29 04:04:33 -0700

    typo

--- packages/stack-shared/src/utils/objects.tsx
+++ packages/stack-shared/src/utils/objects.tsx
@@ -570,7 +570,7 @@ export function deleteKey<T extends object, K extends keyof T>(obj: T, key: K) {
 }
 
 /**
- * Returns truee iff the value is an object or a function, but not null.
+ * Returns true iff the value is an object or a function, but not null.
  */
 export function isObjectLike(value: unknown): value is object | Function {
   return (typeof value === 'object' || typeof value === 'function') && value !== null;
