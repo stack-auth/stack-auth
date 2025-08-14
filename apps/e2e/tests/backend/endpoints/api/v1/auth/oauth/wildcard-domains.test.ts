da6d04e3a256fa29a3ef6210c9b96de15cc01751
Konsti Wohlwend <n2d4xc@gmail.com>
2025-08-13 18:13:07 -0700

    Update apps/e2e/tests/backend/endpoints/api/v1/auth/oauth/wildcard-domains.test.ts

--- apps/e2e/tests/backend/endpoints/api/v1/auth/oauth/wildcard-domains.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/auth/oauth/wildcard-domains.test.ts
@@ -292,7 +292,7 @@ describe("OAuth with wildcard domains", () => {
             baseUrl: 'http://localhost:8107', // This one matches!
             handlerPath: '/handler',
           },
-          'domains.allowLocalhost': true,
+          'domains.allowLocalhost': false,
         }),
       },
     });
