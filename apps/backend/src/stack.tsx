f237e8dd997a7fc9995bf214b0eb9058f18f162e
Bilal Godil <bg2002@gmail.com>
2025-09-05 16:14:13 -0700

    fix env vars

--- apps/backend/src/stack.tsx
+++ apps/backend/src/stack.tsx
@@ -5,6 +5,6 @@ export const stackServerApp = new StackServerApp({
   projectId: 'internal',
   tokenStore: 'memory',
   baseUrl: getEnvVariable('NEXT_PUBLIC_STACK_API_URL'),
-  publishableClientKey: getEnvVariable('STACK_INTERNAL_PROJECT_CLIENT_KEY'),
-  secretServerKey: getEnvVariable('STACK_INTERNAL_PROJECT_SERVER_KEY'),
+  publishableClientKey: getEnvVariable('STACK_SEED_INTERNAL_PROJECT_PUBLISHABLE_CLIENT_KEY'),
+  secretServerKey: getEnvVariable('STACK_SEED_INTERNAL_PROJECT_SECRET_SERVER_KEY'),
 });
