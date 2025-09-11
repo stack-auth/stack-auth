8180ced73e0f149edee84e763b212e0040485755
Bilal Godil <bg2002@gmail.com>
2025-09-11 16:10:22 -0700

    fix schema

--- apps/backend/prisma/migrations/20250911230246_one_time_purchase/migration.sql
+++ apps/backend/prisma/migrations/20250911230246_one_time_purchase/migration.sql
@@ -1,7 +1,6 @@
 -- CreateEnum
 ALTER TYPE "SubscriptionCreationSource" RENAME TO "PurchaseCreationSource";
 
-
 -- CreateTable
 CREATE TABLE "OneTimePurchase" (
     "id" UUID NOT NULL,
@@ -11,8 +10,12 @@ CREATE TABLE "OneTimePurchase" (
     "offerId" TEXT,
     "offer" JSONB NOT NULL,
     "quantity" INTEGER NOT NULL,
+    "stripePaymentIntentId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "creationSource" "PurchaseCreationSource" NOT NULL,
 
     CONSTRAINT "OneTimePurchase_pkey" PRIMARY KEY ("tenancyId","id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OneTimePurchase_tenancyId_stripePaymentIntentId_key" ON "OneTimePurchase"("tenancyId", "stripePaymentIntentId");
