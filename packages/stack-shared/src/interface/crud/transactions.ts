cb4946a6c80751490df9204d2ad4fcad62ca1b4d
Bilal Godil <bg2002@gmail.com>
2025-09-18 10:06:02 -0700

    wip, crud/transactions

--- packages/stack-shared/src/interface/crud/transactions.ts
+++ packages/stack-shared/src/interface/crud/transactions.ts
@@ -5,16 +5,21 @@ export const adminTransaction = yupObject({
   id: yupString().defined(),
   type: yupString().oneOf(["subscription", "one_time", "item_quantity_change"]).defined(),
   created_at_millis: yupNumber().defined(),
+  // created_at
+  // effective_at
   customer_type: customerTypeSchema.defined(),
   customer_id: yupString().defined(),
   quantity: yupNumber().defined(),
   test_mode: yupBoolean().defined(),
-  offer_display_name: yupString().nullable().defined(),
-  price: offerPriceSchema.omit(["serverOnly", "freeTrial"]).nullable().optional(),
+  // offer_json
+  // price_id
+  // offer_display_name: yupString().nullable().defined(),
+  // price: offerPriceSchema.omit(["serverOnly", "freeTrial"]).nullable().optional(),
   status: yupString().nullable().defined(),
+  // item type grant from offer
   item_id: yupString().optional(),
   description: yupString().nullable().optional(),
-  expires_at_millis: yupNumber().nullable().optional(),
+  // expires_at_millis: yupNumber().nullable().optional(),
 }).defined();
 
 export type AdminTransaction = InferType<typeof adminTransaction>;
