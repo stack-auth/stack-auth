c510f29835e4118d6397615f2db9a8ec454a87e4
Bilal Godil <bg2002@gmail.com>
2025-10-10 11:13:40 -0700

    fix

--- examples/convex/stack/server.tsx
+++ examples/convex/stack/server.tsx
@@ -1,7 +1,5 @@
 import { StackServerApp } from "@stackframe/stack";
 
-
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
-  secretServerKey: process.env.STACK_SECRET_SERVER_KEY,
 });
