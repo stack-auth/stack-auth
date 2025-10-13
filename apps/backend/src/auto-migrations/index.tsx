5410be4a74db59c4dbfa2bbafedff04627160dac
Bilal Godil <bg2002@gmail.com>
2025-10-13 10:55:21 -0700

    fix auto migrations

--- apps/backend/src/auto-migrations/index.tsx
+++ apps/backend/src/auto-migrations/index.tsx
@@ -134,7 +134,14 @@ export async function applyMigrations(options: {
 
           const txOrPrismaClient = runOutside ? options.prismaClient : tx;
           if (isSingleStatement) {
-            const res = await txOrPrismaClient.$queryRaw`${Prisma.raw(statement)}`;
+            const statementWithSearchPath = runOutside
+              ? (() => {
+                const trimmedStatement = statement.trimEnd();
+                const ensureSemicolon = trimmedStatement.endsWith(';') ? '' : ';';
+                return `SET search_path TO ${sqlQuoteIdent(options.schema)};\n${statement}${ensureSemicolon}`;
+              })()
+              : statement;
+            const res = await txOrPrismaClient.$queryRaw`${Prisma.raw(statementWithSearchPath)}`;
             if (isConditionallyRepeatMigration) {
               if (!Array.isArray(res)) {
                 throw new StackAssertionError("Expected an array as a return value of repeat condition", { res });
