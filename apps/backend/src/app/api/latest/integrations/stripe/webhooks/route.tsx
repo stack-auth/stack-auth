31a924f33e3ec663dec35f82bcdbb030d76209c2
Bilal Godil <bg2002@gmail.com>
2025-11-18 11:52:23 -0800

    webhook fix

--- apps/backend/src/app/api/latest/integrations/stripe/webhooks/route.tsx
+++ apps/backend/src/app/api/latest/integrations/stripe/webhooks/route.tsx
@@ -100,12 +100,9 @@ async function processStripeWebhookEvent(event: Stripe.Event): Promise<void> {
     const stripe = await getStripeForAccount({ accountId }, mockData);
     await syncStripeSubscriptions(stripe, accountId, customerId);
 
-    if (event.type == "invoice.payment_succeeded") {
+    if (event.type === "invoice.payment_succeeded") {
       await handleStripeInvoicePaid(stripe, accountId, event.data.object);
     }
-  }
-  if (event.type === "refund.created") {
-    const refund = event.data.object;
   }
 }
 
