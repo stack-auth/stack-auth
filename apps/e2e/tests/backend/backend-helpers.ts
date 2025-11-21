1c1d8ca85b8ed656dc441dbf28efe7782d4dbc63
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-11-20 18:11:42 -0800

    fixes

--- apps/e2e/tests/backend/backend-helpers.ts
+++ apps/e2e/tests/backend/backend-helpers.ts
@@ -401,7 +401,7 @@ export namespace Auth {
         if (containsSubstring) {
           break;
         }
-        await wait(200);
+        await wait(100 + i * 10);
         if (i >= 30) {
           throw new StackAssertionError(`Sign-in code message not found after ${i} attempts`, { messages });
         }
