23b2078d9edd49450160f97537f1666834ef773e
Konstantin Wohlwend <n2d4xc@gmail.com>
2026-02-11 20:42:51 -0800

    Increase email retries further

--- apps/backend/src/lib/emails-low-level.tsx
+++ apps/backend/src/lib/emails-low-level.tsx
@@ -300,7 +300,7 @@ export async function lowLevelSendEmailDirectViaProvider(options: LowLevelSendEm
       }
 
       return result;
-    }, 5, { exponentialDelayBase: 1000 });
+    }, 7, { exponentialDelayBase: 125 });
   } catch (error) {
     if (error instanceof DoNotRetryError) {
       return Result.error(error.errorObj);
