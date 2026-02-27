5394614dd361df656a5cbb9add62d85a6093ec8a
Konstantin Wohlwend <n2d4xc@gmail.com>
2026-02-27 10:58:28 -0800

    Upgrade ESLint

--- examples/convex/.eslintrc.js
+++ examples/convex/.eslintrc.js
@@ -4,19 +4,4 @@ module.exports = {
     "../../configs/eslint/next.js",
   ],
   "ignorePatterns": ['/*', '!/src'],
-  rules: {
-    "import/order": [
-      1,
-      {
-        groups: [
-          "external",
-          "builtin",
-          "internal",
-          "sibling",
-          "parent",
-          "index",
-        ],
-      },
-    ],
-  },
 };
