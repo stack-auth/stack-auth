475bdf3fada9de17fb55817e5d4fe3257ad283f5
mantrakp04 <mantrakp@gmail.com>
2026-04-13 17:03:23 -0700

    Add new migrations for anon refresh token and signup email index
    - Created a migration to add the "anonRefreshToken" column to the "CliAuthAttempt" table.
    - Added a new index for "signUpEmailNormalized" on the "ProjectUser" table to improve query performance.
    - Removed obsolete migration that previously handled these changes.

--- apps/backend/prisma/migrations/20260413190209_add_anon_refresh_token_and_signup_email_index/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "CliAuthAttempt" ADD COLUMN     "anonRefreshToken" TEXT;
-
--- CreateIndex
-CREATE INDEX "ProjectUser_signUpEmailNormalized_recent_idx" ON "ProjectUser"("tenancyId", "isAnonymous", "signUpEmailNormalized", "signedUpAt");
-
