c79d3b339737423ef19acbc8e4796e54dc4f966a
Aadesh Kheria <kheriaaadesh@gmail.com>
2026-05-04 12:57:17 -0700

    duplicate import fix

--- packages/stack-cli/src/commands/project.ts
+++ packages/stack-cli/src/commands/project.ts
@@ -1,7 +1,6 @@
 import { Command } from "commander";
-import { resolveLoginConfig, resolveSessionAuth } from "../lib/auth.js";
 import { getInternalUser } from "../lib/app.js";
-import { createProjectInteractively } from "../lib/create-project.js";
+import { resolveLoginConfig, resolveSessionAuth } from "../lib/auth.js";
 import { createProjectInteractively } from "../lib/create-project.js";
 
 export function registerProjectCommand(program: Command) {
