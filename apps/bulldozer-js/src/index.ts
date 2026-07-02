f3122008017dc880ebf3d72cccf0f7847ec2f1ce
Konstantin Wohlwend <n2d4xc@gmail.com>
2026-07-02 16:49:11 -0700

    Better debug logging for Bulldozer globalThis

--- apps/bulldozer-js/src/index.ts
+++ apps/bulldozer-js/src/index.ts
@@ -76,8 +76,9 @@ const bulldozerDb = declareBulldozerDatabase(
   }),
   { migrations: schema.migrations },
 );
-await traceSpan("bulldozer-js.applyRemainingMigrations", async () => await bulldozerDb.applyRemainingMigrations());
 (globalThis as any).bulldozerDb = bulldozerDb;
+console.log("Stored bulldozerDb in globalThis for pid " + process.pid);
+await traceSpan("bulldozer-js.applyRemainingMigrations", async () => await bulldozerDb.applyRemainingMigrations());
 
 function jsonResponse(body: unknown, init?: ResponseInit) {
   return new Response(JSON.stringify(body), {
